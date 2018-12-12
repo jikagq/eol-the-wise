28bfd52f856ce8bda4185a5f13cda909e343436b
jikagq <theo-nec@hotmail.fr>
2018-12-12 16:57:08 +0100

    interpreteur serial ok tx/rx serial ok

--- eolthewise/base_telemetry/base_telemetry.ino
+++ eolthewise/base_telemetry/base_telemetry.ino
@@ -3,10 +3,25 @@
 /*analogue measures*/
 #define TemperaturePin 0
 #define LightPin 1
-#define girouettePin 2
+#define GirouettePin 2
+
+
 
 /*digital measures*/
+#define AnemometerPin 2
+#define PluviometerPin 3
+#define HumiPin 4
 
+#define EncodePin1 4
+#define EncodePin2 5
+#define EncodePin3 6
+#define EncodePin4 7
+
+/*Servo Pins*/
+#define ServoPin1 10
+//#define ServoPin2 11
+
+/*I2C PINS*/
 
 int temp = 0;
 int lum = 0;
@@ -26,7 +41,7 @@ void setup() {
   
   pinMode(TemperaturePin, INPUT);
   pinMode(LightPin, INPUT);
-  pinMode(girouettePin, INPUT);
+  pinMode(GirouettePin, INPUT);
   
 }
 
@@ -35,31 +50,20 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  
-  
-  /*digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.println("off");
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  Serial.println("on");// wait for a second
-  delay(1000);*/
-
-
   if(flag_trame_recu == 1){//check si une trame valide a été reçu
       digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        //interpreteur();
-      Serial.println("recu: "+Trame_RX);
+      interpreteur();//traitement
+      //Serial.println("recu: "+Trame_RX);
      reset_all();//une fois traitement fini reset
     }else{
           digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
       }
-  
+   // delay(100);
 }
 
 void serialEvent(){
   if (Serial.available() > 0) {
         incomingChar = Serial.read();  // read the incoming byte:
-       
         if(incomingChar == ';'){//fin de trame
           flag_trame_recu=1;
         }else{//ajout du caractere à la suite de la chaine
@@ -68,88 +72,67 @@ void serialEvent(){
         }
     }
 }
-/*void interpreteur(void){
-    char c = '\0';
-    c = Trame_uart_rx[0]; //1er caractere reçu indique l'action à effectuer
 
+void interpreteur(void){
+    char c = '\0';
+    c = Trame_RX.charAt(0);
     switch(c){
           case '0' :{//mode test test
-              TXframe("Frame detected");
-              TXframe("Ground telemetry ok");
-              TXframe("Test ok :)");
+              Serial.println("Base station telemetry V2");
+              Serial.println("Serial Frame detected");
+              Serial.println("Ground telemetry ok");
+              Serial.println("Communication test ok :)");
+              reset_all();
               break;
           }
+          case 'p':{//controle pwm servo
+               //controle_servo();//p:1,255;
+//               Control_servo();
+               reset_all();
+               break;
+          }
           case 's' :{//send all
-                update_valeurs(&data_mesures);
-
-                itoad(get_angle(&data_mesures), &tab_temp, 10);
-                ajout_trame_uart('g',&tab_temp,&Trame_uart_tx);
-                TXframe(&Trame_uart_tx);
-                reset_uart();
-
-                itoad(get_humi(&data_mesures), &tab_temp, 10);
-                ajout_trame_uart('h',&tab_temp,&Trame_uart_tx);
-                TXframe(&Trame_uart_tx);
-                reset_uart();
-
-                itoad(get_lum(&data_mesures), &tab_temp, 10);
-                ajout_trame_uart('l',&tab_temp,&Trame_uart_tx);
-                TXframe(&Trame_uart_tx);
-                reset_uart();
-
-                itoad(get_temp(&data_mesures), &tab_temp, 10);
-                ajout_trame_uart('t',&tab_temp,&Trame_uart_tx);
-                TXframe(&Trame_uart_tx);
-                reset_uart();
+                update_valeurs();
+                Serial.println(send_all_measure());
+                reset_all();
                 break;
-                  }
+          }
           case 'u':{//mise à jour des capteurs
-              update_valeurs(&data_mesures);
-              ack();
+              update_valeurs();
+              Serial.println("ACK");
+              reset_all();
               break;
-                 }
+          }
           case 'g':{//girouette
-             itoad(get_angle(&data_mesures), &tab_temp, 10);
-             ajout_trame_uart('g',&tab_temp,&Trame_uart_tx);
-             TXframe(&Trame_uart_tx);
-             reset_uart();
+              Trame_TX = "g:"+ get_angle();
+              Serial.println(Trame_TX);
+              reset_all();
               break;
-                 }
-          case 'h':{//humidité
-               itoad(get_humi(&data_mesures), &tab_temp, 10);
-               ajout_trame_uart('h',&tab_temp,&Trame_uart_tx);
-               TXframe(&Trame_uart_tx);
-               reset_uart();
-              break;
-               }
-           case 'l':{//luminosité
-               itoad(get_lum(&data_mesures), &tab_temp, 10);
-                ajout_trame_uart('l',&tab_temp,&Trame_uart_tx);
-                TXframe(&Trame_uart_tx);
-                reset_uart();
+          }
+          case 'l':{//luminosité
+               Trame_TX = "l:"+ get_lum();
+               Serial.println(Trame_TX);
+               reset_all();
                break;
                }
            case 't':{//température
-               itoad(get_temp(&data_mesures), &tab_temp, 10);
-                ajout_trame_uart('t',&tab_temp,&Trame_uart_tx);
-                TXframe(&Trame_uart_tx);
-                reset_uart();
+               Trame_TX = "t:"+ get_temp();
+               Serial.println(Trame_TX);
+               reset_all();
                break;
                }
-           case 'p':{//controle pwm servo
-               controle_servo();//p:1,255;
-               reset_uart();
-
-               //statut_pwm = ; //0= pas de pwm 1=pwm en sortie
-               //pwm = ; //valeur de la pwm
-               break;
-               }
+          /* case 'h':{//humidité
+               //suite
+              reset_all();
+              break;
+               }*/
            default :{
-               nak();
+               Serial.println("NAK");
                break;
                }
           }
-}*/
+}
+
 void reset_all(void){
   incomingChar = '\0';   // for incoming serial data
   Trame_RX ="";
@@ -157,8 +140,25 @@ void reset_all(void){
   flag_trame_recu =0;
 }
 
-void send_all_data(void){
-  
+String  send_all_measure(void){
+   String str1 = get_angle();
+   String str2 = get_lum();
+   String str3 = get_temp();
+   return "g:"+ str1 +" l:"+str2+" t:"+str3 ;
+}
+
+
+/*accede aux valeurs traiter/envoyer*/
+String get_angle(void){
+    return String(gir);
+}
+String get_temp(void){
+    
+    return String(temp);
+}
+String get_lum(void){
+    
+    return String(lum);
 }
 
 void update_valeurs(void){
@@ -167,9 +167,9 @@ void update_valeurs(void){
    lum  = get_luminosite();
 }
 
-/*lit l'adc*/
+/*analogues measures*/
 int get_girouette(void){  
-    return analogRead(girouettePin) ;
+    return analogRead(GirouettePin) ;
 }
 int get_temperature(void){   
     return analogRead(TemperaturePin);
@@ -177,5 +177,51 @@ int get_temperature(void){
 int get_luminosite(void){
     return analogRead(LightPin);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*digital measure*/
+int get_anemometer(){
+  
+}
+int get_pluvio(){
+  
+}
+int get_encoder(){
+
+}
+int get_humi(){
+  
+}
+/*I2C power measure*/
+void get_INA219_values(){
+  
+}
+
+/*Control_servo(void){
+  bool etat = 0;
+  int pwm = 0;
+
+  Trame_RX.charAt(2);
+  //analogWrite(pinDEL, luminosite);
+
+  
+}*/
 
 
